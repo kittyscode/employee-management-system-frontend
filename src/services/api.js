2b46231df649cb9c0ba55e05a2b0e532adc3fd72
Kirti Dhangar <kirtidhangar2002@gmail.com>
2026-07-28 17:08:09 +0530

    Api updation

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://localhost:8082/api",
+    // baseURL: "http://localhost:8082/api",
+
+    baseURL: "employee-management-system-production-af73.up.railway.app",
 
     headers: {
         "Content-Type": "application/json",
