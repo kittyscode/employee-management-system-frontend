1321272faef97e54da9643969130c11eaad7aa49
Kirti Dhangar <kirtidhangar2002@gmail.com>
2026-08-01 13:40:45 +0530

    Fix employee api

--- src/components/profile/ProfilePhoto.jsx
+++ src/components/profile/ProfilePhoto.jsx
@@ -37,7 +37,7 @@ const ProfilePhoto = ({ profile, setProfile }) => {
 
 
             const response = await axios.post(
-                "${import.meta.env.VITE_API_BASE_URL}/profile/upload-image",
+                `${import.meta.env.VITE_API_BASE_URL}/profile/upload-image`,
                 formData,
                 {
                     headers:{
@@ -57,7 +57,7 @@ const ProfilePhoto = ({ profile, setProfile }) => {
 
 
             const updatedProfile = await axios.get(
-    "${import.meta.env.VITE_API_BASE_URL}/profile",
+    `${import.meta.env.VITE_API_BASE_URL}/profile`,
     {
         headers:{
             Authorization:
